7bc85b4f21fd5531e52c394e08f34e203fd98d97
Marina Rozova <marinar578@gmail.com>
2016-08-29 23:12:01 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+// MODULE
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+
+// ROUTES
+weatherApp.config(function($routeProvider){
+
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'homeController'
+    })
+
+    .when('/forecast', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'
+    })
+
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'
+    })
+
+});
+
+// SERVICES
+weatherApp.service('cityService', function(){
+
+    this.city = 'New York, NY';
+
+});
+
+
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
+
+    $scope.city = cityService.city; 
+
+    // have angular watch if the $scope changes and update the service's city value if it does
+    $scope.$watch('city', function() {
+        cityService.city = $scope.city;
+    });
+
+
+
+}]);
+
+
+weatherApp.controller('forecastController', ['$scope', 'cityService', '$resource', '$routeParams', function($scope, cityService, $resource, $routeParams){
+
+    $scope.city = cityService.city; 
+
+    $scope.days = $routeParams.days || '2';
+
+    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily?APPID=680ebba65700dc221535345af27939f8', {callback: "JSON_CALLBACK"}, { get: {method: "JSONP"}});
+
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days  });
+
+    $scope.convertToFahrenheight = function(degK){
+
+        return Math.round((1.8 * (degK - 273)) + 32);
+
+    }
+
+    $scope.convertToDate = function(dt){
+        return new Date(dt * 1000);
+    }
+}]);
+
+
+// weatherApp.directive('', function(){
+//     return{
+
+//     }
+// });
+
