6a602eed18a034aad6453c6c1a03c5ef12de57c0
Marina Rozova <marinar578@gmail.com>
2016-09-02 00:03:50 -0400

    Separate directives into new file

--- controllers.js
+++ controllers.js
@@ -8,6 +8,25 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
         cityService.city = $scope.city;
     });
 
+}]);
 
+weatherApp.controller('forecastController', ['$scope', 'cityService', '$resource', '$routeParams', function($scope, cityService, $resource, $routeParams){
 
+    $scope.city = cityService.city; 
+
+    $scope.days = $routeParams.days || '2';
+
+    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily?APPID=680ebba65700dc221535345af27939f8', {callback: "JSON_CALLBACK"}, { get: {method: "JSONP"}});
+
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days  });
+
+    $scope.convertToFahrenheight = function(degK){
+
+        return Math.round((1.8 * (degK - 273)) + 32);
+
+    }
+
+    $scope.convertToDate = function(dt){
+        return new Date(dt * 1000);
+    }
 }]);
