f2a47ec8b37a32d585e14f615df9e69e7f14b4da
Marina Rozova <marinar578@gmail.com>
2016-09-01 23:59:23 -0400

    Separate routes into new file

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
 // MODULE
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
-
-// ROUTES
-weatherApp.config(function($routeProvider){
-
-    $routeProvider
-
-    .when('/', {
-        templateUrl: 'pages/home.html',
-        controller: 'homeController'
-    })
-
-    .when('/forecast', {
-        templateUrl: 'pages/forecast.html',
-        controller: 'forecastController'
-    })
-
-    .when('/forecast/:days', {
-        templateUrl: 'pages/forecast.html',
-        controller: 'forecastController'
-    })
-
-});
-
 // SERVICES
 weatherApp.service('cityService', function(){
 
